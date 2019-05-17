1959ee9c1190c350c93b156326d0edb124e4395a
Anne QUAN <aquan@e3r6p8.42.fr>
2019-05-17 17:18:00 +0200

    gallery en cours

---
+++ gallery.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include_once("navigation.php");
+include_once("user_functions.php");
+include_once("db.php");
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>CAMAGRU</title>
+  <link rel="stylesheet" href="gallery.css">	
+  <script src="script.js"></script>
+</head>
+
+
+<body>
+ <div class="container">
+            <div class="header"></div>
+            <div class="menu"></div>
+            <div class="content">
+                <div><img src="logo_hdr/chat1.jpg"/></div>
+                <div><img src="logo_hdr/chat4.jpg"/></div>
+                <div><img src="logo_hdr/chatRobinet.jpg"/></div>
+                <div><img src="logo_hdr/chat-siamois.jpg"/></div>
+
+    	
+
+
+
+		
+	
+
+	
+	
+            </div>
+
+            <div class="footer">ABOUT US . SUPPORT . PRESS . API . JOBS . PRIVACY . TERMS . DIRECTORY . PROFILES . HASHTAGS . LANGUAGE</div>
+        </div>
+  
+</body>
+</html>
