025c02d21896b0a0ee8415b31d06ad5ad0a005b2
Louise P <cpieri@student.42.fr>
2019-05-21 16:50:05 +0200

    [Feature]: Comment And photo ok

--- gallery.php
+++ gallery.php
@@ -6,21 +6,17 @@ include_once("db.php");
 
 // logged_only();
 if ($_SESSION['auth']){
-  // var_dump($_POST['comment']);
   if(!empty($_POST['comment'])){
 
-    $user_id = $_SESSION['auth']->id;
+	$user_id = $_SESSION['auth']->id;
+	$pic_id = (int)htmlspecialchars($_POST['pic_id']);
     $comments = htmlspecialchars($_POST['comment']);
 
-    $bdd->prepare('INSERT INTO comment SET comments = ?, uid = ?')->execute([$comments, $user_id]);
+    $bdd->prepare('INSERT INTO comments SET comment = ?, uid = ?, photo_id = ?')->execute([$comments, $user_id, $pic_id]);
     header('Location: gallery.php');
-    // $_SESSION['flash']['success'] = "comments add";
-  }
+	$_SESSION['flash']['success'] = "comments add";
 }
-// else{
-//   header('Location: gallery.php');
-//   // exit();
-// }
+}
 
 ?>
 
@@ -29,73 +25,47 @@ if ($_SESSION['auth']){
 <head>
   <meta charset="utf-8">
   <title>Gallery</title>
-  <link rel="stylesheet" href="gallery.css">	
+  <link rel="stylesheet" href="gallery.css">
   <script src="script.js"></script>
 </head>
 
 
 <body>
- <div class="container">
-            <div class="header"></div>
-            <div class="menu"></div>
-            <div class="content">
-            <form action="" method="post">
-            <div class="gallery">
-           
-                <div class="comments">
-                <?php if (!($_SESSION['auth'])){ ?>
-                  <div class="nop"> <a href="login.php"><?php echo "Log in to like or comment"; ?></a> </div>
-               <?php }
-                
-                 else { ?>
-                      <input name="comment" type="text" placeholder= "Add a comment..."><input type=submit Value="Done">
-                    <?php  }?>
-                   <div class="text">
-                      <?php
-                      $reponse = $bdd->query('SELECT comment FROM comments');
-                      $donnees = $reponse->fetchAll(PDO::FETCH_COLUMN, 'comments');
-                      foreach($donnees as $commentaire)
-                      {
-                      ?>
-                      <p class="comment"> <?php echo $commentaire ?> </p>
-                        <?php } ?>
-                      
-          
-                   </div>
-                </div>
+	<div class="container">
+		<div class="header"></div>
+		<div class="menu"></div>
+		<div class="content">
+		<?php
+			$rep = $bdd->query('SELECT photo, id FROM photos');
+			$pic = $rep->fetchAll();
 
-                <?php
-                      $rep = $bdd->query('SELECT photo FROM photos');
-                      $pic = $rep->fetchAll(PDO::FETCH_COLUMN, 'photos');
-                      foreach($pic as $data)
-                      { 
-                      ?>
-                      <img src="data:image/jpg;base64,<?php echo $data; ?>"/>
-                <?php } ?>
-                
-             
-                
-          
-            </div>
-            </form>
-                <div class="gallery"><div class="comments"><p class="text">Ce chat est merveilleux</p></div><img src="logo_hdr/chat1.jpg"/></div>
-                <div class="gallery"><div class="comments"><p class="text">Ce chat est merveilleux</p></div><img src="logo_hdr/chat4.jpg"/></div>
-                <div class="gallery"><div class="comments"><p class="text">Ce chat est merveilleux</p></div><img src="logo_hdr/chatRobinet.jpg"/></div>
-               
-               
-    	
+			foreach($pic as $data) {
+				$reponse = $bdd->prepare('SELECT comment FROM comments INNER JOIN photos ON comments.photo_id = photos.id WHERE photos.id = ?');
+				$reponse->execute([$data->id]);
+				$donnees = $reponse->fetchAll(PDO::FETCH_COLUMN, 'comments');
 
-
-
-		
-	
-
-	
-	
-            </div>
-
-            <div class="footer">ABOUT US . SUPPORT . PRESS . API . JOBS . PRIVACY . TERMS . DIRECTORY . PROFILES . HASHTAGS . LANGUAGE</div>
+				echo "<form action='' method='post'>";
+				echo "<div id='$data->id' class='gallery'>";
+				echo "<div class='comments'>";
+                if (!($_SESSION['auth'])) {
+                	echo "<div class='nop'><a href='login.php'>Log in to like or comment</a></div>";
+				} else {
+					echo "<input name='pic_id' value='$data->id' type='hidden'>";
+                    echo "<input name='comment' type='text' placeholder= 'Add a comment...'><input type=submit Value='Done'>";
+				}
+				echo "<div class='text'>";
+				foreach($donnees as $commentaire) {
+                      echo "<p class='comment'>$commentaire</p>";
+				}
+				echo "</div>";
+				echo "</div>";
+				echo "<img src='data:image/jpg;base64, $data->photo'/>";
+				echo "</div>";
+				echo "</form>";
+			}
+		?>
+		</div>
+		<div class="footer">ABOUT US . SUPPORT . PRESS . API . JOBS . PRIVACY . TERMS . DIRECTORY . PROFILES . HASHTAGS . LANGUAGE</div>
         </div>
-  
 </body>
 </html>
