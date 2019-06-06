39a1f33c4b1ec12e128c5df5658eaca84a35b3c2
Anne QUAN <aquan@e3r7p2.42.fr>
2019-06-06 19:37:10 +0200

    db ok

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$bdd = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', 'root', 'camagru');
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-?>
